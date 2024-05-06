72611150c7262740e2589978f39cffef9c66159e
ashshelby <nepalidude3@gmail.com>
2024-05-07 01:52:44 +0545

    changes

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from image_generator import InviteGenerator
-import functions
+# import functions
+from person import Person
 
 
 def main():
-    print("Hello World!")
+    # print("Hello World!")
     generator = InviteGenerator("./invitation.png")
-    for person in functions.get_participants():
-        generator.create(person)
+    # for person in functions.get_participants():
+    #     generator.create(person)
         # print(person.first_name)
+    
+    sanjay = Person(uuid=None, first_name="Sanjay",last_name="Pahari",email="paharisanjay10@gmail.com",team=None)
+
+    generator.create(sanjay)
 
 
 if __name__ == "__main__":
